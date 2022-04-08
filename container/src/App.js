bd201d0cb1144894d25895ccfc360bc877996ee2
Darren Scrine <darren.s@mmtdigital.co.uk>
2022-04-08 12:53:16 +0100

    initial commit

---
+++ container/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MarketingApp from './components/MarketingApp';
+
+export default () => {
+  return (
+    <div>
+      <h1>This is the Container App!</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+};
